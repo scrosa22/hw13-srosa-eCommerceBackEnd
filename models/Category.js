6bc121efab00150c5df4ccb3e65b48803177816d
steven rosa <scrosa13@outlook.com>
2022-08-22 20:02:25 -0400

    updating models

---
+++ models/Category.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTERGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // onDelete: 'CASCADE'
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
